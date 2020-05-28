5adb296f5761e00c16e50b1bcf0fd53c50d30464
Arnaldo Quintero <arnaldoquinteo96@gmail.com>
2020-05-28 21:40:05 +0100

    creado test con valores no enteros

--- pruebas.py
+++ pruebas.py
@@ -6,6 +6,12 @@ class CalculatorTest(unittest.TestCase):
 
     def test_suma_enteros(self):
         self.assertEqual(4, self.calculator.add(2,2))
+    
+    def test_suma_invalido_negativo(self):
+        self.assertRaises(ArithmeticError, self.calculator.add, -2, 2)
+    
+    def test_suma_invalido_negativo_2(self):
+        self.assertRaises(ArithmeticError, self.calculator.add, 2, -2)
 
 if __name__ == "__main__":
     unittest.main()
