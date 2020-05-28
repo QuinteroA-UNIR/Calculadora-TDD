cf4d58e213ba6a132453c7979007e79e9f6329f6
Arnaldo Quintero <arnaldoquinteo96@gmail.com>
2020-05-28 21:42:47 +0100

    creacion metodo resta

--- Calculator.py
+++ Calculator.py
@@ -3,3 +3,5 @@ class Calculator:
         if (op1 < 0 or op2 < 0):
             raise ArithmeticError("No se permiten valores negativos")
         return op1 + op2
+    def minus(self, op1, op2):
+        return op1 - op2
