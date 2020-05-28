fd33da281692626144e8f1ba057f521e1d7dd01b
Arnaldo Quintero <arnaldoquinteo96@gmail.com>
2020-05-28 20:12:24 +0100

    creacion fichero calculator

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,11 @@
 import unittest
+from Calculator import Calculator
 
 class CalculatorTest(unittest.TestCase):
     calculator = Calculator()
 
     def test_suma_enteros(self):
         self.assertEqual(4, self.calculator.add(2,2))
+
+if __name__ == "__main__":
+    unittest.main()
