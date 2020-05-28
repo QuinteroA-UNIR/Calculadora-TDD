9f77bbdcd29f8736ea8f82fe5ff340abafff6b86
Arnaldo Quintero <arnaldoquinteo96@gmail.com>
2020-05-28 21:45:13 +0100

    validacion de enteros en multiplicacion

--- Calculator.py
+++ Calculator.py
@@ -8,6 +8,8 @@ class Calculator:
             raise ArithmeticError("No se permiten valores negativos")
         return op1 - op2
     def multiple(self, op1, op2):
+        if (op1 < 0 or op2 < 0):
+            raise ArithmeticError("No se permiten valores negativos")
         result = 0
         for i in range(op2):
             result += op1
