cb18c2973e8486e8d309c5c6024d1d41e5ced056
Arnaldo Quintero <arnaldoquinteo96@gmail.com>
2020-05-28 21:43:09 +0100

    validacion de enteros en metodo resta

--- Calculator.py
+++ Calculator.py
@@ -4,4 +4,6 @@ class Calculator:
             raise ArithmeticError("No se permiten valores negativos")
         return op1 + op2
     def minus(self, op1, op2):
+        if (op1 < 0 or op2 < 0):
+            raise ArithmeticError("No se permiten valores negativos")
         return op1 - op2
